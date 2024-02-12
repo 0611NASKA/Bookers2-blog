2620a4113aa6c29747287047cf2627c9025377a0
0611NASKA <010611naoki@gmail.com>
2024-02-12 14:56:53 +0900

    画像投稿機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,14 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_one_attached :image
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
